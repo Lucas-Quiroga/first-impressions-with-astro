c94844b59e4d7b844515840e5028064c26cbb510
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-01-03 11:41:12 -0300

    completed

---
+++ src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import { useState } from "preact/hooks";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <>
+      <span class="text-yellow-300 text-xl mr-4">{counter}</span>
+      <button
+        class="border px-4 py-2 text-xl"
+        onClick={() => setCounter((counter) => counter + 1)}
+      >
+        +
+      </button>
+      <button
+        class="border px-4 py-2 text-xl"
+        onClick={() => setCounter((counter) => counter - 1)}
+      >
+        -
+      </button>
+    </>
+  );
+};
+
+export default Counter;
